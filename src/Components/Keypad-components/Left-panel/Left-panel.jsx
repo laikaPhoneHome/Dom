ce74828a0875cc0faab4b658fe23e561b3fd1c95
Robyn <clockhands9@gmail.com>
2022-10-26 09:02:08 +0100

    merge

--- src/Components/Keypad-components/Left-panel/Left-panel.jsx
+++ src/Components/Keypad-components/Left-panel/Left-panel.jsx
@@ -2,13 +2,14 @@ import MU from './Mark-up';
 import Ambivalence from './Ambivalence';
 import SQR from './Square-root';
 import CE from './Clear-entry';
+import SQRT from './Square-root';
 
 const Leftpanel =({handleInput}) => {
     return (
         <section className='left-buttons'>
             <MU handleInput={handleInput}/>
             <Ambivalence />
-            <SQR handleInput={handleInput}/>
+            <SQRT handleInput={handleInput}/>
             <CE />
         </section>
     )
